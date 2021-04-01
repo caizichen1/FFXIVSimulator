b00489569dc6ae8b730279b3226459a485875b7d
careston <caizichen@xdfzx.com>
2021-04-01 16:31:13 +0800

    feat: 修改文件头

--- src/routers/index.js
+++ src/routers/index.js
@@ -1,11 +1,15 @@
 /*
- * @LastEditors: caizichen
+ * @Description: 
+ * @Author: caizichen1
+ * @Date: 2021-03-31 16:19:02
+ * @LastEditors: caizichen1
+ * @LastEditTime: 2021-04-01 15:12:11
  */
 import Loadable from 'react-loadable';
 
 const page = component => {
   return Loadable({
-    loader: () => import(`./../pages/${component}`),
+    loader: () => import(`./../pages/${component}/index.jsx`),
     loading() {
       return <div>Loading...</div>
     }
@@ -16,7 +20,7 @@ const routes = [
   // 默认路由
   { path: '/', component: page('index') },
   { path: '/index', component: page('index') },
-  { path: '/demo', component: page('demo') },
+  { path: '/simpleton', component: page('simpleton') },
 ];
 
 export default routes;
