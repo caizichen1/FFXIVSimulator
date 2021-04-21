6a844b429da00c06b19e9307f19f2fd994912576
careston <caizichen@xdfzx.com>
2021-04-21 11:06:04 +0800

    feat: 优化分摊核爆的效果

--- src/pages/index/index.jsx
+++ src/pages/index/index.jsx
@@ -3,11 +3,11 @@
  * @Author: caizichen1
  * @Date: 2021-03-31 16:19:02
  * @LastEditors: caizichen1
- * @LastEditTime: 2021-04-01 15:11:46
+ * @LastEditTime: 2021-04-21 10:38:41
  */
 import React from 'react';
 import './index.scss'
-// import RangeDecay from '../../components/methods/rangeDecay'
+import RangeDecay from '../../components/methods/rangeDecay'
 import DamageShare from '../../components/methods/damageShare'
 export default class Index extends React.Component {
 
@@ -15,6 +15,7 @@ export default class Index extends React.Component {
     return (
       <div className='container'>
         <DamageShare />
+        {/* <RangeDecay /> */}
       </div>
     );
   }
