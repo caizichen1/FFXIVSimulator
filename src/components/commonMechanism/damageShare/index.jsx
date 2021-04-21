4e65c6d2c131552ec32919cb06ba94397dac9f3b
GUI <541349328@qq.com>
2021-04-21 20:14:08 +0800

    feat: 机制组件文件重命名

--- src/components/commonMechanism/damageShare/index.jsx
+++ src/components/commonMechanism/damageShare/index.jsx
@@ -1,13 +1,11 @@
 import React, { Component } from 'react'
-import Role from '../../role'
 import './index.scss'
 
 export default class DamageShare extends Component {
     render() {
         return (
-            <div className="damageshare-compoent">
+            <div className="damageshare-component">
                 <div className="circle">
-                    <Role />
                     <div className="arrow-box bottom">
                         <div className="arrow one"></div>
                         <div className="arrow two"></div>
--- src/components/commonMechanism/rangeDecay/index.jsx
+++ src/components/commonMechanism/rangeDecay/index.jsx
@@ -1,12 +1,10 @@
 import React, { Component } from 'react'
-import Role from '../../role'
 import './index.scss'
 
 export default class RangeDecay extends Component {
     render() {
         return (
-            <div className="rangedecay">
-                <Role />
+            <div className="rangedecay-component">
                 <div className="arrow bottom"></div>
                 <div className="arrow left"></div>
                 <div className="arrow right"></div>
