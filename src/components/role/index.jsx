980085469206a552ea38029564fd543ecd6b7b70
GUI <541349328@qq.com>
2021-04-21 20:08:10 +0800

    feat: 修改角色组件和机制组件的从属关系

--- src/components/role/index.jsx
+++ src/components/role/index.jsx
@@ -1,36 +1,74 @@
 import React from 'react';
+import DamageShare from './../commonMechanism/damageShare';
+import RangeDecay from './../commonMechanism/rangeDecay';
 import './index.scss'
+
 
 export default class Role extends React.Component {
 
+  state = {
+    // 用于确定职业身份
+    roles: [
+      {
+        profession: 'T',
+        name: 'MT',
+      },
+      {
+        profession: 'T',
+        name: 'ST',
+      },
+      {
+        profession: 'D',
+        name: 'D1',
+      },
+      {
+        profession: 'D',
+        name: 'D2',
+      },
+      {
+        profession: 'D',
+        name: 'D3',
+      },
+      {
+        profession: 'D',
+        name: 'D4',
+      },
+      {
+        profession: 'H',
+        name: 'H1',
+      },
+      {
+        profession: 'H',
+        name: 'H2',
+      },
+
+    ]
+  }
+
   render() {
+    const role = this.state.roles.find((r) => {
+      return r.name === this.props.role
+    }) || {
+      profession: 'T',
+      name: 'MT',
+    }
+
+    const { mechanism } = this.props
+
     return (
       <div className="roles">
-        <div className="role T">
-          <span>MT</span>
+        <div className={`role ${role.profession}`}>
+          <span>{role.name}</span>
+
+          {
+            mechanism === '伤害分摊' ? <DamageShare /> :
+              mechanism === '距离衰减' ? <RangeDecay /> :
+                console.log("此时角色无作用机制！")
+          }
+
         </div>
-        {/* <div className="role T">
-          <span>ST</span>
-        </div>
-        <div className="role D">
-          <span>D1</span>
-        </div>
-        <div className="role D">
-          <span>D2</span>
-        </div>
-        <div className="role D">
-          <span>D3</span>
-        </div>
-        <div className="role D">
-          <span>D4</span>
-        </div>
-        <div className="role H">
-          <span>H1</span>
-        </div>
-        <div className="role H">
-          <span>H2</span>
-        </div> */}
       </div>
     );
   }
 }
+
